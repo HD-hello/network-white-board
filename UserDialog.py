8134b31b7459f1e1c39299ee91bc7de6381ba3c8
hhh <123456@qq.com>
2020-01-07 10:39:48 +0800

    画图

--- UserDialog.py
+++ UserDialog.py
@@ -3,9 +3,18 @@ from tkinter import *
 class UserDialog:
     _Ip = ''
     _port = 0
+    _nickname = ''
 
     def __init__(self):
         pass
+
+    @classmethod
+    def show_error_box(cls, msg):
+        master = Tk()
+        Label(master, text=msg).grid(row=0)
+        Button(master, text='OK', command=master.destroy).grid(row=1, pady=4)
+        master.mainloop()
+
 
     @classmethod
     def getUserInputIp(cls):
@@ -29,6 +38,26 @@ class UserDialog:
         button = Button(ClientWindow)
         button.config(text='ok',command = getUserIpAndPort)
         button.grid(row = 3, column= 0)
+
+        ClientWindow.mainloop()
+
+    @classmethod
+    def getUserNickName(cls):
+        def getUserNickNameInner():
+            cls._nickname = e1.get()
+
+            ClientWindow.destroy()
+
+        ClientWindow = Tk()
+        Label(ClientWindow, text='请输入昵称').grid(row=0)
+        Label(ClientWindow, text='昵称').grid(row=1)
+
+        e1 = Entry(ClientWindow)
+        e1.grid(row=1, column=1)
+
+        button = Button(ClientWindow)
+        button.config(text='ok', command=getUserNickNameInner)
+        button.grid(row=2, column=0)
 
         ClientWindow.mainloop()
 
--- client.py
+++ client.py
@@ -1,21 +1,62 @@
-import socket
+import time
+from threading import Thread
 
-from UserDialog import UserDialog
+from connection import Connection
+from whiteboard import WhiteBoard
 
 
-class Connection:
+class Client(Thread,WhiteBoard):
+
     def __init__(self):
-        UserDialog.getUserInputIp()
-        self.host = UserDialog._Ip
-        self.port = UserDialog._port
-        print(self.host,self.port)
+        self.conn = Connection()
+        Thread.__init__(self)
+        WhiteBoard.__init__(self)
+        self._init_mouse_event()
+        self.setDaemon(True)
+        self.isMouseDown = False
+        self.x_pos = None
+        self.y_pos = None
 
-        self.sock = socket.socket()
-        self.sock.connect((self.host,self.port))
+    def _init_mouse_event(self):
+        self.drawing_area.bind("<Motion>", self.motion)
+        self.drawing_area.bind("<ButtonPress-1>", self.left_but_down)
+        self.drawing_area.bind("<ButtonRelease-1>", self.left_but_up)
 
 
+    def left_but_down(self,event=None):
+        self.isMouseDown = True
+        self.x_pos = event.x
+        self.y_pos = event.y
+
+
+    def left_but_up(self,event=None):
+        self.isMouseDown = False
+        print(event.x,event.y)
+
+
+    def motion(self,event=None):
+        if self.isMouseDown == True:
+            msg = ('D',self.x_pos,self.y_pos,event.x,event.y,'red')
+            self.conn.send_message(msg)
+            self.x_pos = event.x
+            self.y_pos = event.y
+
+
+    def run(self):
+        # print('run')aa
+        while True:
+            msg = self.conn.receive_msg()
+            if msg == 'xxx':
+                pass
+            # print('i am running')
+            time.sleep(0.1)
 
 if __name__ == '__main__':
-    conn = Connection()
+    client = Client()
+    print('startt')
+    client.start()
+    client.show_window()
 
-    print('start')
+
+
+
